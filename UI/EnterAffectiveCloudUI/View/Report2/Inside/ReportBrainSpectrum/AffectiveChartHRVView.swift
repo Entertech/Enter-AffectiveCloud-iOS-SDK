dfa7e693e4101cd60f1abaf02742394b328cce60
ET-LINK <ke.liful@gmail.com>
2020-04-30 10:00:29 +0800

    Fix: 缩小hrv的y轴最小显示间隔

--- UI/EnterAffectiveCloudUI/View/Report2/Inside/ReportBrainSpectrum/AffectiveChartHRVView.swift
+++ UI/EnterAffectiveCloudUI/View/Report2/Inside/ReportBrainSpectrum/AffectiveChartHRVView.swift
@@ -290,7 +290,7 @@ public class AffectiveChartHRVView: UIView, ChartViewDelegate {
         var maxLabel = 0
         var minLabel = 0
         var bScaleIs2 = false
-        if tempMax5 - tempMin5 < 20 {
+        if tempMax5 - tempMin5 < 10 {
             let tempMax2 = (maxValue / 2 + 1) * 2 > 100 ? 100 : (maxValue / 2 + 1) * 2
             let tempMin2 = (minValue / 2 ) * 2 < 0 ? 0 : (minValue / 2) * 2
             maxLabel = tempMax2
