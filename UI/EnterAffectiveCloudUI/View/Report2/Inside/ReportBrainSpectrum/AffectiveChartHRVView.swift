7a631656cef1281c0b5b581620b3a824bc2b8f0d
ET-LINK <ke.liful@gmail.com>
2020-09-04 15:23:40 +0800

    Fix：HRV算法修改，报表数据变为两倍

--- UI/EnterAffectiveCloudUI/View/Report2/Inside/ReportBrainSpectrum/AffectiveChartHRVView.swift
+++ UI/EnterAffectiveCloudUI/View/Report2/Inside/ReportBrainSpectrum/AffectiveChartHRVView.swift
@@ -103,7 +103,7 @@ public class AffectiveChartHRVView: UIView, ChartViewDelegate, UIGestureRecogniz
     private var sample = 3
     private var maxDataCount = 100
     private let mainFont = "PingFangSC-Semibold"
-    private let interval = 0.4
+    private let interval = 0.2
     private var timeStamp = 0
     private var hrvArray: [Int]?
     private var yRender: LimitYAxisRenderer?
