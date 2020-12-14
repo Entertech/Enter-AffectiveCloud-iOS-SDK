29294d175e29afee42b576c85b9532f556e729f0
ET-LINK <ke.liful@gmail.com>
2020-12-14 18:08:13 +0800

    fix: 修复按钮位置

--- UI/EnterAffectiveCloudUI/View/Report2/Inside2/AffectiveRhythmsView.swift
+++ UI/EnterAffectiveCloudUI/View/Report2/Inside2/AffectiveRhythmsView.swift
@@ -186,46 +186,46 @@ public class AffectiveRhythmsView: UIView, ChartViewDelegate {
         gamaBtn.backgroundColor = gamaColor.changeAlpha(to: 0.2)
         gamaBtn.setTitle("γ", for: .normal)
         gamaBtn.setTitleColor(gamaColor, for: .normal)
-        gamaBtn.layer.cornerRadius = 14
+        gamaBtn.layer.cornerRadius = 12
         gamaBtn.adjustsImageWhenHighlighted = false
-        gamaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
-        gamaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        gamaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        gamaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
         gamaBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         
         betaBtn.backgroundColor = gamaColor.changeAlpha(to: 0.2)
         betaBtn.setTitle("β", for: .normal)
         betaBtn.setTitleColor(betaColor, for: .normal)
-        betaBtn.layer.cornerRadius = 14
+        betaBtn.layer.cornerRadius = 12
         betaBtn.adjustsImageWhenHighlighted = false
-        betaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
-        betaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        betaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        betaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
         betaBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         
         alphaBtn.backgroundColor = alphaColor.changeAlpha(to: 0.2)
         alphaBtn.setTitle("α", for: .normal)
         alphaBtn.setTitleColor(alphaColor, for: .normal)
-        alphaBtn.layer.cornerRadius = 14
+        alphaBtn.layer.cornerRadius = 12
         alphaBtn.adjustsImageWhenHighlighted = false
-        alphaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
-        alphaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        alphaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        alphaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
         alphaBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         
         thetaBtn.backgroundColor = thetaColor.changeAlpha(to: 0.2)
         thetaBtn.setTitle("θ", for: .normal)
         thetaBtn.setTitleColor(thetaColor, for: .normal)
-        thetaBtn.layer.cornerRadius = 14
+        thetaBtn.layer.cornerRadius = 12
         thetaBtn.adjustsImageWhenHighlighted = false
-        thetaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
-        thetaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        thetaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        thetaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
         thetaBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         
         deltaBtn.backgroundColor = deltaColor.changeAlpha(to: 0.2)
         deltaBtn.setTitle("δ", for: .normal)
         deltaBtn.setTitleColor(deltaColor, for: .normal)
-        deltaBtn.layer.cornerRadius = 14
+        deltaBtn.layer.cornerRadius = 12
         deltaBtn.adjustsImageWhenHighlighted = false
-        deltaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
-        deltaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        deltaBtn.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 5)
+        deltaBtn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: -5)
         deltaBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         
         minLabel.textColor = textColor
@@ -268,7 +268,7 @@ public class AffectiveRhythmsView: UIView, ChartViewDelegate {
             $0.left.equalToSuperview().offset(16)
             $0.right.equalToSuperview().offset(-16)
             $0.top.equalToSuperview().offset(8)
-            $0.height.equalTo(28)
+            $0.height.equalTo(32)
         }
         
         chartView.snp.makeConstraints {
