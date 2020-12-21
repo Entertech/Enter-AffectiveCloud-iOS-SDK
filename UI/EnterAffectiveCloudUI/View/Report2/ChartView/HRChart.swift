e0af5e648c7d4df3e7ab6797c070d3db9d5ca939
ET-LINK <ke.liful@gmail.com>
2020-12-21 22:33:36 +0800

    no message

--- UI/EnterAffectiveCloudUI/View/Report2/ChartView/HRChart.swift
+++ UI/EnterAffectiveCloudUI/View/Report2/ChartView/HRChart.swift
@@ -362,8 +362,8 @@ class HRChart: LineChartView {
         self.xAxis.valueFormatter = HRVXValueFormatter(timeApart, timeStamp)
         
         if maxScreenCount > 0 {
-            //let range = Double(maxScreenCount) / Double(valueCount * sample) * self.xRange > self.xRange ? self.xRange : Double(maxScreenCount) / Double(valueCount * sample) * self.xRange
-            //self.setVisibleXRangeMaximum(range)
+            let range = Double(maxScreenCount) / Double(valueCount * sample) * self.xRange > self.xRange ? self.xRange : Double(maxScreenCount) / Double(valueCount * sample) * self.xRange
+            self.setVisibleXRangeMaximum(range)
             let value = Double(paddingIndex)/Double(valueCount)*self.xRange
             self.moveViewToX(value-30)
             
