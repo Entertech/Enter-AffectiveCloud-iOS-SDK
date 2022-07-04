fd56458f7655a8b3e9f00137dbf3b802dca25f16
ET-LINK <ke.liful@gmail.com>
2022-07-04 17:36:10 +0800

    no message

--- UI/EnterAffectiveCloudUI/View/Report3/AffectiveCharts3BarCommonView.swift
+++ UI/EnterAffectiveCloudUI/View/Report3/AffectiveCharts3BarCommonView.swift
@@ -80,8 +80,8 @@ extension AffectiveCharts3BarCommonView: AffectiveCharts3ExpandDelegate {
                 sv?.setContentOffset(CGPoint(x: 0, y: 36), animated: true)
                 sv?.isScrollEnabled = false
                 chartView.snp.updateConstraints {
-                    $0.leading.equalTo(80)
-                    $0.trailing.equalTo(80)
+                    $0.leading.equalToSuperview().offset(80)
+                    $0.trailing.equalToSuperview().offset(80)
                     $0.bottom.equalToSuperview().offset(-32)
                 }
                 parent.snp.updateConstraints {
@@ -98,8 +98,8 @@ extension AffectiveCharts3BarCommonView: AffectiveCharts3ExpandDelegate {
                 sv?.setContentOffset(.zero, animated: true)
                 view.transform = CGAffineTransform(rotationAngle: CGFloat(0))
                 chartView.snp.updateConstraints {
-                    $0.leading.equalTo(0)
-                    $0.trailing.equalTo(0)
+                    $0.leading.equalToSuperview().offset(0)
+                    $0.trailing.equalToSuperview().offset(0)
                     $0.bottom.equalToSuperview().offset(-8)
                 }
                 view.frame.origin.y = 0
