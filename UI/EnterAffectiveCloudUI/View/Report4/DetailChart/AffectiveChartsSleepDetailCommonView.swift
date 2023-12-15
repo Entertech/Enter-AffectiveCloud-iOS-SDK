4746cea355b08e0f5f37fcf722a998d0b224ea09
LK-LINK <ke.liful@gmail.com>
2023-12-15 16:37:06 +0800

    Revert "fix: 修改高度"
    This reverts commit e1e775773f43a2d1c582f163d5fbe37b5f45a266.

--- UI/EnterAffectiveCloudUI/View/Report4/DetailChart/AffectiveChartsSleepDetailCommonView.swift
+++ UI/EnterAffectiveCloudUI/View/Report4/DetailChart/AffectiveChartsSleepDetailCommonView.swift
@@ -56,7 +56,6 @@ public class AffectiveChartsSleepDetailCommonView: LineChartView {
         scaleYEnabled = false
         legend.enabled = false
         highlightPerTapEnabled = false
-
         
     }
 }
--- UI/EnterAffectiveCloudUI/View/Report4/DetailChart/AffectiveChartsSleepStageView.swift
+++ UI/EnterAffectiveCloudUI/View/Report4/DetailChart/AffectiveChartsSleepStageView.swift
@@ -87,15 +87,15 @@ public class AffectiveChartsSleepStageView: UIView {
         }
         remLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(12)
-            $0.top.equalTo(awakeLabel.snp.bottom).offset(12)
+            $0.top.equalTo(awakeLabel.snp.bottom).offset(14)
         }
         lightLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(12)
-            $0.top.equalTo(remLabel.snp.bottom).offset(12)
+            $0.top.equalTo(remLabel.snp.bottom).offset(14)
         }
         deepLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(12)
-            $0.top.equalTo(lightLabel.snp.bottom).offset(12)
+            $0.top.equalTo(lightLabel.snp.bottom).offset(14)
         }
         return self
     }
